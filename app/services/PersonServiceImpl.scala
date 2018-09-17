63675cdd6a16921b74c755022ed88975ccc44940
Tatiana Zapata Cano <tatiana.zapata@ceiba.com.co>
2018-09-17 17:58:25 -0500

    Retos de slick

--- app/services/PersonServiceImpl.scala
+++ app/services/PersonServiceImpl.scala
@@ -1,17 +1,21 @@
 package services
 
 import javax.inject.{Inject, Singleton}
+import tables.PersonsTable
 
 import scala.concurrent.Future
 
 @Singleton
 class PersonServiceImpl @Inject() (dbConfigProvider: DatabaseConfigProvider) extends PersonService {
-
+  import profile.api._
+  private val persons = TableQuery[PersonsTable]
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
 
-  def crear(person: Person): Future[Int]{
+  def crear(person: Person): Future[Int] = db.run(
+    persons.map(p =>
+      (p.name, p.lastName)) += (person.name, person.lastName)
+  )
 
-  }
   def consultar(idPerson: Long): Future[Person]{
 
   }
--- app/tables/PersonTable.scala
+++ app/tables/PersonTable.scala
@@ -5,7 +5,10 @@ import slick.jdbc.MySQLProfile.api._
 import slick.lifted.ProvenShape
 
 class PersonsTable(tag: Tag) extends Table[Person](tag, "persons") {
-  def id: Rep[Option[Long]] = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
-  def name: Rep[String] = column[String]("name")
-  def lastName: Rep[String] = column[String]("name")
+  def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name")
+  def lastName = column[String]("name")
+
+  //def * : ProvenShape[Person] = (id, name, lastName) <> ((Person.apply _).tupled, Person.unapply)
+  def * = (id, name, lastName) <> (Person.tupled, Person.apply _, Person.unapply _)
 }
