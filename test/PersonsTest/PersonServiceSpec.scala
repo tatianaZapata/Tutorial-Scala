2061cb23bea6d36b7e2677ce8e701586e3a44e11
Tatiana <tatiana.zapata@ceiba.com.co>
2018-10-12 17:28:07 -0500

    Se realizan las pruebas unitarias

--- test/PersonsTest/PersonServiceSpec.scala
+++ test/PersonsTest/PersonServiceSpec.scala
@@ -1,54 +1,56 @@
 package PersonsTest
 
 import models.Person
-import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Application
-import play.api.inject.bind
-import play.api.inject.guice.GuiceApplicationBuilder
 import services.PersonService
 import scala.concurrent.Future
-import org.scalatest.Matchers._
+import org.mockito.Mockito._
 
 class PersonServiceSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerSuite with ScalaFutures{
 
-  override def fakeApplication(): Application = new GuiceApplicationBuilder()
-    .overrides(bind[PersonService].to[PersonServiceMock])
-    .build()
+  val mockPersonService = mock[PersonService]
 
-  val personService = app.injector.instanceOf(classOf[PersonService])
-
-  "PersonService#crear" should {
+  "\n\nPersonService#CREAR" should {
     "Se creó una persona correctamente" in {
-      // val personServices = mock[PersonServiceImpl]
-      val person = mock[Person]
-      val futureResult: Future[Int] = personService.crear(person)
-      val result = futureResult.futureValue
-      // result should equal(1)
-      assert( result == 1)
+      //Arrange
+        val person = mock[Person]
+        when(mockPersonService.crear(person: Person)) thenReturn Future.successful(1)
+      //Act
+        val futureResult: Future[Int] = mockPersonService.crear(person)
+        val result = futureResult.futureValue
+      //Assert
+        assert( result == 1)
     }
   }
 
-  "PersonService#consultar" should {
-    "No existe la persona consultada" in {
-      val idPerson = 1L
-      val person = mock[Person]
-      val futureResult: Future[Option[Person]] = personService.consultar(idPerson)
-      val result = futureResult.futureValue
-      // result should equal(None)
-      assert( result == None)
+  "\nPersonService#CONSULTAR" should {
+    "Persona consultada correctamente" in {
+      //Arrange
+        val idPerson = 2L
+        val person = mock[Person]
+        when(mockPersonService.consultar(idPerson)) thenReturn Future.successful(Option(person))
+      //Act
+        val futureResult: Future[Option[Person]] = mockPersonService.consultar(idPerson)
+        val result = futureResult.futureValue
+      //Assert
+        result mustBe a [Option[Person]]
     }
   }
 
-  "PersonService#listar" should {
+  "\nPersonService#LISTAR" should {
     "Lista de personas" in {
-      val person = mock[Person]
-      val futureResult: Future[Seq[Person]] = personService.listar()
-      val result = futureResult.futureValue
-      result should have length 0
+      //Arrange
+        val person1, person2 = mock[Person]
+        val listPersons: List[Person] = List(person1, person2)
+        when(mockPersonService.listar()) thenReturn Future.successful(listPersons)
+      //Act
+        val futureResult: Future[Seq[Person]] = mockPersonService.listar()
+        val result = futureResult.futureValue
+      //Assert
+        result must have length 2
     }
   }
 }
